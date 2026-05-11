52eaab4700bd3adad21d520bb728dbfe8c697d5e
Carlote12 <carloaugustocatalini@gmail.com>
2026-05-10 22:52:11 -0300

    aaa working on secret animation alien game

---
+++ script.js
@@ -0,0 +1,11 @@
+const personaje = document.getElementById('player');
+
+personaje.addEventListener('click', () => {
+    personaje.classList.add('correr-derecha');
+    
+    if (personaje.classList.contains('correr-derecha')) {
+        console.log("Estado: Corriendo");
+    } else {
+        console.log("Estado: Quieto");
+    }
+});
