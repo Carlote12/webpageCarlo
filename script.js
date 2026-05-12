a812f7d3dbc26a742ea442aa50ecb812bbc702f8
Carlote12 <carloaugustocatalini@gmail.com>
2026-05-12 15:06:56 -0300

    funciona traducción

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+document.querySelectorAll('[data-language]').forEach(btn => {
+    btn.addEventListener('click', async () => {
+        const lang = btn.dataset.language;
+        
+        try {
+            // Buscamos el archivo
+            const response = await fetch(`./lenguajes/${lang}.json`);
+            
+            if (!response.ok) throw new Error(`${lang}.json (Error ${response.status})`);
+            
+            // Convertimos a JSON
+            const data = await response.json();
+
+            document.querySelectorAll('[data-section]').forEach(el => {
+                const sec = el.dataset.section;
+                const val = el.dataset.value;
+                if(data[sec] && data[sec][val]) el.innerText = data[sec][val];
+            });
+
+        } catch (error) {
+            console.error(`[ERROR CRÍTICO]:`, error);
+            alert(`No se pudo cargar "${lang}"`);
+        }
+    });
+});
+
 const personaje = document.getElementById('player');
 
 personaje.addEventListener('click', () => {
@@ -9,4 +35,3 @@ personaje.addEventListener('click', () => {
         console.log("Estado: Quieto");
     }
 });
-
